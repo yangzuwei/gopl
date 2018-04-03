de6eac548ca464286f1a40782bdd48ec2c6b8738
yang <316193839@qq.com>
2018-04-03 17:30:37 +0800

    增加了echo服务器和netcat2客户端，但是客户端不符合预期效果

---
+++ src/echo/echo.go
@@ -0,0 +1,46 @@
+// echo project echo.go
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"net"
+	"strings"
+	"time"
+)
+
+//func handleConn(c net.Conn) {
+//	io.Copy(c, c)
+//	c.Close()
+//}
+
+func echo(c net.Conn, shout string, delay time.Duration) {
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		echo(c, input.Text(), 1*time.Second)
+	}
+	c.Close()
+}
+
+func main() {
+	listener, err := net.Listen("tcp", "0.0.0.0:8000")
+	if err != nil {
+		fmt.Println(err)
+	}
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		handleConn(conn)
+	}
+}
