9e8bb4bda4bd8a7a37d16c263a0e44a66f311a93
yang <316193839@qq.com>
2018-04-17 17:28:10 +0800

    degree like c

---
+++ src/degree/degree.go
@@ -0,0 +1,17 @@
+// degree project degree.go
+package main
+
+import (
+	"flag"
+	"fmt"
+	"time"
+)
+
+var period = flag.Duration("period", 1*time.Second, "sleep period")
+
+func main() {
+	flag.Parse()
+	fmt.Printf("Sleeping for %v ...", *period)
+	time.Sleep(*period)
+	fmt.Println()
+}
